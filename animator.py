23261d6199b5ae49b52efaa1409e8528b2fe228e
SappieKonig <70659954+SappieKonig@users.noreply.github.com>
2020-12-06 20:10:58 +0100

    Update animator.py

--- animator.py
+++ animator.py
@@ -9,21 +9,22 @@ import numpy as np
 class animator:
 
     def __init__(self, max_history=5, max_loss=None):
+        # we keep a history of all the losses of the last 5 epochs
         self.max_history = max_history
+        
+        # creating the graph, which gets displayed in the push function
         self.fig = go.FigureWidget()
         self.fig.add_scatter()
+        
+        # we start with an empty history
         self.history = list(np.zeros(max_history))
-        self.max_loss = max_loss
-        self.has_run = False
 
     def push(self, loss):
-        if self.max_loss == None:
-            if self.has_run == False:
-                self.max_loss = loss
-            else:
-                Exception("Why the fuck has it not run yet?")
+        # we add our new loss to the history, whilst removing the last
         self.history = self.history[1:]
         self.history += [loss]
+        
+        # here we update the graph and display it in the browser
         with self.fig.batch_update():
             self.fig.data[0].y = self.history
             self.fig.show()
